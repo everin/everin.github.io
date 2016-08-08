f97efcfb223ff74c84506f0ede05fcc2f5803c8c
Everin Orlandi @W <everin.orlandi@gmail.com>
2016-08-08 16:05:59 +0200

    Multiple controllers on the same page

--- js/controllers.js
+++ js/controllers.js
@@ -1,28 +1,34 @@
-var simpleController =	function ($scope){
-	$scope.name = "HelloController";
-	$scope.hello = "Hello from HelloController";
+var app = angular.module('app', ['simple', 'hello']);
+var simple = angular.module('simple', []);
+var hello = angular.module('hello', []);
+
+simple.controller('SimpleController', function ($scope){
+	$scope.name = "SimpleController";
+	$scope.vendors = [{name:'Simone',city:'Trento',debt:'180.01',birthDate:'1088323623006'},{name:'Andrea',city:'Verona',debt:'13441.80',birthDate:'1888323623006'}];
 
 	$scope.getName = function() {
 		return $scope.name;
 	};
-};
 
-var helloController = function ($scope){
+	$scope.clickName = function() {
+		alert('SIMPLE CLICK: '.concat($scope.name));
+	};
+
+});
+
+hello.controller('HelloController', function ($scope){
 	$scope.name = "HelloController";
-	$scope.hello = "Hello from HelloController";
 
 	$scope.getName = function() {
 		return $scope.name;
 	};
-};
+	
+	$scope.clickName = function() {
+		alert('HELLO CLICK: '.concat($scope.name));
+	};
+});
 
-var firstController = function ($scope){
-  // Initialize the model variables
-  $scope.firstName = "John";
-  $scope.lastName = "Doe";
+function genericClick(){
+	alert('ciao da genericClick');
+}
 
-  // Define utility functions
-  $scope.getFullName = function () {
-		return $scope.firstName + " " + $scope.lastName;
-  };
-};
