68465f7cf9c9a14622a1fc2d6960e6e9d0332b34
Everin Orlandi @Home <everin.orlandi@gmail.com>
2016-08-11 09:58:53 +0200

    Added in-page controller and divided JS files with multiple controller

--- js/SimpleController.js
+++ js/SimpleController.js
@@ -1,13 +1,14 @@
-(function(angular) {
-	'use strict';
-	angular.module('controllerExample', []).controller('SimpleController', ['$scope', SimpleController]);
+var simple = angular.module('simple', []);
 
-	function SimpleController($scope) {
-		$scope.name = "SimpleController";
-		$scope.vendors = [{name:'Simone',city:'Trento',debt:'180.01',birthDate:'1088323623006'},{name:'Andrea',city:'Verona',debt:'13441.80',birthDate:'1888323623006'}];
+simple.controller('SimpleController', function ($scope){
+	$scope.name = 'SimpleController';
+	$scope.vendors = [{name:'Simone',city:'Trento',debt:'180.01',birthDate:'1088323623006'},{name:'Andrea',city:'Verona',debt:'13441.80',birthDate:'1888323623006'}];
 
-		$scope.getName = function() {
-			return name;
-		};
-	}
-})(window.angular);
+	$scope.getName = function() {
+		return $scope.name;
+	};
+
+	$scope.clickName = function() {
+		alert('SIMPLE CLICK: '.concat($scope.name));
+	};
+});
--- js/controllers.js
+++ js/controllers.js
@@ -1,23 +1,8 @@
-var app = angular.module('app', ['simple', 'hello']);
-var simple = angular.module('simple', []);
+var app = angular.module('app', ['simple', 'hello', 'inPage']);
 var hello = angular.module('hello', []);
 
-simple.controller('SimpleController', function ($scope){
-	$scope.name = "SimpleController";
-	$scope.vendors = [{name:'Simone',city:'Trento',debt:'180.01',birthDate:'1088323623006'},{name:'Andrea',city:'Verona',debt:'13441.80',birthDate:'1888323623006'}];
-
-	$scope.getName = function() {
-		return $scope.name;
-	};
-
-	$scope.clickName = function() {
-		alert('SIMPLE CLICK: '.concat($scope.name));
-	};
-
-});
-
 hello.controller('HelloController', function ($scope){
-	$scope.name = "HelloController";
+	$scope.name = 'HelloController';
 
 	$scope.getName = function() {
 		return $scope.name;
